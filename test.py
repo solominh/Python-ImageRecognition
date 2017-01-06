20f6b4226e0582bee760470d4158115b75006ac6
solominh <hoangminhftu2@gmail.com>
2017-01-06 16:51:43 +0700

    Test Threashold => perfect

--- test.py
+++
@@ -1,6 +0,0 @@
-import cv2
-im_gray = cv2.imread('./grayscale_image.png')
-(thresh, im_bw) = cv2.threshold(im_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-# thresh = 127
-# im_bw = cv2.threshold(im_gray, thresh, 255, cv2.THRESH_BINARY)[1]
-cv2.imwrite('blackwhite_image.png', im_bw)
---
+++ test_threshold.py
@@ -0,0 +1,11 @@
+import cv2
+im_gray = cv2.imread('./grayscale_image.png')
+# (thresh, im_bw) = cv2.threshold(im_gray, 128,
+                                # 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+thresh = 210
+im_bw = cv2.threshold(im_gray, thresh, 255, cv2.THRESH_BINARY)[1]
+
+# show the image
+cv2.imshow("Image", im_bw)
+cv2.waitKey(0)
+cv2.imwrite('blackwhite_image.png', im_bw)
