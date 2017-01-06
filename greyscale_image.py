723c2894a41b70da0cfc7320a942119f227864f1
solominh <hoangminhftu2@gmail.com>
2017-01-06 16:15:50 +0700

    Clean up

--- greyscale_image.py
+++ greyscale_image.py
@@ -3,8 +3,8 @@ import os
 print(cv2.__version__)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-image_path=os.path.join(BASE_DIR,'shapes_and_colors.png')
-image_rel_path='./shapes_and_colors.png'
+image_path=os.path.join(BASE_DIR,'typing_game_image.png')
+image_rel_path='./typing_game_image.png'
 image = cv2.imread(image_rel_path)
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imwrite('gray.jpg',gray_image)
