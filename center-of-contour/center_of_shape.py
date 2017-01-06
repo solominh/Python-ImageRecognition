899c45a1255fcd5640385b545f9add75feab175c
solominh <hoangminhftu2@gmail.com>
2017-01-06 15:26:14 +0700

    Add more features

--- center-of-contour/center_of_shape.py
+++ center-of-contour/center_of_shape.py
@@ -6,16 +6,18 @@ import argparse
 import imutils
 import cv2
 
-image_path='./typing_game_image.png'
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to the input image")
+args = vars(ap.parse_args())
+
 # load the image, convert it to grayscale, blur it slightly,
 # and threshold it
-image = cv2.imread(image_path)
+image = cv2.imread(args["image"])
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
-
-cv2.imshow("Image", blurred)
-cv2.waitKey(0)
 
 # find contours in the thresholded image
 cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
